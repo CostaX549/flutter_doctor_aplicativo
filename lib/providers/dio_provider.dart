9cd4d379e95d36a539130f75db8ee8ff40bae72f
Gabriel Santos Costa <138066007+CostaX549@users.noreply.github.com>
2024-09-30 23:26:50 -0300

    Projeto Flutter até 30/09/2024 23:26

---
+++ lib/providers/dio_provider.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class DioProvider {
+  Future<dynamic> getToken(String email, String password) async {
+    try {
+      var response  = await Dio().post("http://192.168.0.207/api/login",
+       data: {'email': email, 'password': password}
+      );
+      if(response.statusCode == 200 && response.data != '') {
+        final SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setString("token", response.data);
+        return true;
+      } else {
+        return false;
+      }
+    } catch(error) {
+      return error;
+    }
+  }
+  Future<dynamic> getUser(String token) async {
+    try {
+      var user = await Dio().get("http://192.168.0.207/api/user",
+       options: Options(headers: {'Authorization': 'Bearer $token'})
+      );
+       if(user.statusCode == 200 && user.data != '') {
+        return json.encode(user.data);
+      }
+    } catch(error) {
+      return error;
+    }
+  }
+
+  Future<dynamic> registerUser(String username, String email, String password) async {
+    try {
+      var user = await Dio().post("http://localhost/api/register",
+          data: {'name': username,'email': email, 'password': password}
+      );
+    
+       if(user.statusCode == 201 && user.data != '') {
+        return true;
+      } else {
+        return false;
+      }
+    } catch(error) {
+      return error;
+    }
+  }
+}
